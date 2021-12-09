5ff6262d3f73241748bb00d212750a6c2dcc0783
Julian Swerdlin <julianswer36@gmail.com>
2021-12-10 00:16:20 +0100

    uploading new files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Toaster, toast} from 'react-hot-toast';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+
+      <button></button>
+
+      <Toaster />
+    </div>
+  )
+}
