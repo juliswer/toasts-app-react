307fbd898d0ab373347625ce80b8cf30d322a34e
Julian Swerdlin <julianswer36@gmail.com>
2021-12-14 22:26:17 +0100

    uploading files

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <button onClick={() => toast('Hello world')}>Hello</button>
 
       <Toaster 
-        position="bottom-left"
+        position="bottom-right"
       />
     </div>
   )
